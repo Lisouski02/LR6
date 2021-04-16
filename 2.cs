281acd0a8fd16a4ddaddb28e96b2fe40ea50d481
Николай Лисовский <82660054+Lisouski02@users.noreply.github.com>
2021-04-16 21:25:58 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // xxxxxxxxx x.x.
+
+            decimal s = 0;
+            Console.WriteLine("Введите последовательность из 11 символов");
+            s = Convert.ToDecimal(Console.ReadLine());
+
+
+            string result = String.Format("{0:######### (#)(#)}", s);
+
+            Console.WriteLine(result);
+
+
+
+
+        }
+    }
+}
